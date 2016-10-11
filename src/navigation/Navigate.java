4b4467bd0cd412300f8b45e528276284ce77a769
Ahmet Akgul <akgulahmet@outlook.com>
2016-10-11 15:52:43 -0400

    Implement method to calculate the angle needed to turn

--- src/navigation/Navigate.java
+++ src/navigation/Navigate.java
@@ -1,23 +1,105 @@
 package navigation;
 
+import lejos.hardware.motor.EV3LargeRegulatedMotor;
+import lejos.hardware.sensor.EV3UltrasonicSensor;
+
 public class Navigate extends Thread {
 	
-	public Navigate(){
+	private double currentX;
+	private double currentY;
+	private double currentTheta;
+	private double destinationX;
+	private double destinationY;
+	private EV3LargeRegulatedMotor leftMotor;
+	private EV3LargeRegulatedMotor rightMotor;
+	private EV3UltrasonicSensor usSensor;
+	private Odometer odometer;
+	private double wheelRadius;
+	private double track;
+	private Position[] destination;
+	private boolean isNavigating = false;
+	
+	public Navigate(EV3LargeRegulatedMotor leftMotor, EV3LargeRegulatedMotor rightMotor
+			, EV3UltrasonicSensor usSensor, Odometer odometer, double wheelRadius, double track,
+			Position[] destination){
+		this.leftMotor = leftMotor;
+		this.rightMotor = rightMotor;
+		this.usSensor = usSensor;
+		this.odometer = odometer;
+		this.wheelRadius = wheelRadius;
+		this.track = track;
+		this.destination = destination;
+		
+		for (EV3LargeRegulatedMotor motor : new EV3LargeRegulatedMotor[] { leftMotor, rightMotor }) {
+			motor.stop();
+			motor.setAcceleration(200);
+		}
 		
 	}
 	
-	public void travelTo(double x, double y){
+	public void run(){
+		try {
+			Thread.sleep(2000);
+		} catch (InterruptedException e) {
+			// there is nothing to be done here because it is not expected that
+			// the odometer will be interrupted by another thread
+		}
+		for (int i = 0; i<destination.length; i++){
+			travelTo(destination[i]);
+		}
+	}
+	
+	public void travelTo(Position position){
+		isNavigating = true;
+		double thetad = 0;
+		double diffX = position.getX() - odometer.getX();
+		double diffY = position.getY() - odometer.getY();
+		
+		if (diffY!=0){
+			if (diffY>0){
+				thetad = Math.atan(diffX/diffY);
+			}
+			else if (diffY<0){
+				if (diffX<0)
+					thetad = Math.atan(diffX/diffY) - Math.PI;
+				if (diffX>0)
+					thetad = Math.atan(diffX/diffY) + Math.PI;
+			}
+		} else if (diffY==0){
+			if (diffX>0)
+				thetad = Math.PI/2;
+			if (diffX<0)
+				thetad = -Math.PI/2;
+			if (diffX==0)
+				thetad = 0;
+		}
+		
+		double thetar = odometer.getTheta();
+		
+		double diffTheta = thetad - thetar;
+		
+		if (diffTheta<-Math.PI/2)
+			diffTheta += 2* Math.PI;
+		if (diffTheta>Math.PI/2)
+			diffTheta -= 2* Math.PI;
+		
+		turnTo(diffTheta);
+		
+		
+		isNavigating = false;
+		
 		
 	}
 	
 	public void turnTo(double theta){
+		isNavigating = true;
 		
 		
 	}
 	
 	public boolean isNavigating(){
 		
-		return true;
+		return isNavigating;
 	}
 	
 
